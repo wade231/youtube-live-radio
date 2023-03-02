590bfc16f66fd4a189d91e4a6dee2aaeb6181483
wadenui9 <126170422+wadenui9@users.noreply.github.com>
2023-03-02 15:57:07 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,11 +4,5 @@ set -e
 
 while true
 do
-  ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i bg.gif \
-    -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+ ffmpeg -re -stream_loop -1 -i bg.mp4 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://a.rtmp.youtube.com/live2/513d-xphe-p2qt-v3w0-4bp3
 done
