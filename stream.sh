f9bfc0affc899ab816db370fd30ecb6c9fbb2ce0
wade231 <122469077+wade231@users.noreply.github.com>
2023-03-06 10:01:28 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
- ffmpeg -re -stream_loop -1 -i video4.mp4 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://a.rtmp.youtube.com/live2/yd4q-9m2v-3wdc-yphu-1ccw
+ ffmpeg -re -stream_loop -1 -i video4.mp4 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://a.rtmp.youtube.com/live2/vgzf-xs76-6xf4-uwx9-dvav
 done
